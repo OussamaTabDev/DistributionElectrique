c1394ab3d036d0bb9c20c32a967f326b426c9087
OussamaTabDev <oussamatabzioui09@gmail.com>
2025-04-18 10:34:53 +0100

    new Updated

--- VersionAlpha/main.py
+++ VersionAlpha/main.py
@@ -1,56 +1,60 @@
-<<<<<<< HEAD
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-cols = ["Result" , "C" , "B" , "A" , "Ia" , "Ib" , "Ic" , "Va" , "Vb" , "Vc"]
-df = pd.read_csv("classData.csv" )
-# print(df.head())
+# <<<<<<< HEAD
+# import pandas as pd 
+# import numpy as np 
+# import matplotlib.pyplot as plt
+# from sklearn.preprocessing import StandardScaler
+# cols = ["Result" , "C" , "B" , "A" , "Ia" , "Ib" , "Ic" , "Va" , "Vb" , "Vc"]
+# df = pd.read_csv("classData.csv" )
+# # print(df.head())
 
-# df["Result"]
+# # df["Result"]
 
-# for label in cols[:-1]:
-#     plt.hist(df[df["Result"] == 1][label] , color= 'blue' , label="Positive" , alpha = 0.6 , density = True )
-#     plt.hist(df[df["Result"] == 0][label] , color= 'red' , label="Negative" , alpha = 0.6 , density = True )
-#     plt.title(label)
-#     plt.ylabel("P(X)")
-#     plt.xlabel(label)
-#     plt.legend()
-#     plt.show()
-# print(len(X))
+# # for label in cols[:-1]:
+# #     plt.hist(df[df["Result"] == 1][label] , color= 'blue' , label="Positive" , alpha = 0.6 , density = True )
+# #     plt.hist(df[df["Result"] == 0][label] , color= 'red' , label="Negative" , alpha = 0.6 , density = True )
+# #     plt.title(label)
+# #     plt.ylabel("P(X)")
+# #     plt.xlabel(label)
+# #     plt.legend()
+# #     plt.show()
+# # print(len(X))
 
-train , valid , test = np.split(df.sample(frac = 1) , [int(0.6 * len(df)) , int(0.8 * len(df))])
+# train , valid , test = np.split(df.sample(frac = 1) , [int(0.6 * len(df)) , int(0.8 * len(df))])
 
-print(len(train[train["Result" == 1]]))
+# print(len(train[train["Result" == 1]]))
 
-# x = dataframe[dataframe.cols[:-1]].values
-# y = dataframe[dataframe.cols[:-1]].values
-=======
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-cols = ["Result" , "C" , "B" , "A" , "Ia" , "Ib" , "Ic" , "Va" , "Vb" , "Vc"]
-df = pd.read_csv("classData.csv" )
-# print(df.head())
+# # x = dataframe[dataframe.cols[:-1]].values
+# # y = dataframe[dataframe.cols[:-1]].values
+# =======
+# import pandas as pd 
+# import numpy as np 
+# import matplotlib.pyplot as plt
+# from sklearn.preprocessing import StandardScaler
+# cols = ["Result" , "C" , "B" , "A" , "Ia" , "Ib" , "Ic" , "Va" , "Vb" , "Vc"]
+# df = pd.read_csv("classData.csv" )
+# # print(df.head())
 
-# df["Result"]
+# # df["Result"]
 
-# for label in cols[:-1]:
-#     plt.hist(df[df["Result"] == 1][label] , color= 'blue' , label="Positive" , alpha = 0.6 , density = True )
-#     plt.hist(df[df["Result"] == 0][label] , color= 'red' , label="Negative" , alpha = 0.6 , density = True )
-#     plt.title(label)
-#     plt.ylabel("P(X)")
-#     plt.xlabel(label)
-#     plt.legend()
-#     plt.show()
-# print(len(X))
+# # for label in cols[:-1]:
+# #     plt.hist(df[df["Result"] == 1][label] , color= 'blue' , label="Positive" , alpha = 0.6 , density = True )
+# #     plt.hist(df[df["Result"] == 0][label] , color= 'red' , label="Negative" , alpha = 0.6 , density = True )
+# #     plt.title(label)
+# #     plt.ylabel("P(X)")
+# #     plt.xlabel(label)
+# #     plt.legend()
+# #     plt.show()
+# # print(len(X))
 
-train , valid , test = np.split(df.sample(frac = 1) , [int(0.6 * len(df)) , int(0.8 * len(df))])
+# train , valid , test = np.split(df.sample(frac = 1) , [int(0.6 * len(df)) , int(0.8 * len(df))])
 
-print(len(train[train["Result" == 1]]))
+# print(len(train[train["Result" == 1]]))
 
-# x = dataframe[dataframe.cols[:-1]].values
-# y = dataframe[dataframe.cols[:-1]].values
->>>>>>> 86a1d048fcc34558fa29d8ee935901de522837e9
+# # x = dataframe[dataframe.cols[:-1]].values
+# # y = dataframe[dataframe.cols[:-1]].values
+# >>>>>>> 86a1d048fcc34558fa29d8ee935901de522837e9
  
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3])
+plt.title('Matplotlib Line Plot Example')
+plt.show()
